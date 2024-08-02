a03a73a0776f9573853909c95926c60224f924c1
Pranjal Rai <pranjalrai1@gmail.com>
2024-08-02 17:20:12 +0530

    complete project

---
+++ src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Logo({width = '100px'}) {
+  return (
+    <div>
+      <img style={{"width":"100px", "height":"100px"}} src='https://www.onlinelogomaker.com/blog/wp-content/uploads/2017/07/hiking-logo-design.jpg' />
+    </div>
+  )
+}
+
+export default Logo
